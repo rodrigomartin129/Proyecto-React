5b0bf88e92af75ea410780e242091d844b3f6805
rodrigomartin129 <martin.rodrigo.gerardo@gmail.com>
2023-08-22 20:10:01 -0300

    first commit

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,13 @@
+import carro from './assets/Carro_compras.svg'
+import './CartWidget.css'
+
+const CartWidget = () => {
+    return (
+        <div>
+            <img src={carro} alt="cartwidget"/>
+            0
+        </div>
+    )
+}
+
+export default CartWidget
